244eff929b5cecb0e8be8443d3d253cf525f37da
Arthur Cristo <arthur.cristo2804@gmail.com>
2024-07-12 16:55:00 -0300

    feat: create a specific calculate function, make simpleCalculate more readable

--- app/src/main/java/com/arthur/gascalculator/AlcoholOrGasCalculator.kt
+++ app/src/main/java/com/arthur/gascalculator/AlcoholOrGasCalculator.kt
@@ -4,25 +4,48 @@ import java.text.NumberFormat
 import java.util.Locale
 
 class AlcoholOrGasCalculator {
-    fun genericCalculate(alcoholValue: String, gasValue: String): String {
+    fun simpleCalculate(alcoholValue: String, gasValue: String): String {
         val format: NumberFormat = NumberFormat.getInstance(Locale.getDefault())
         var result: String
         try {
-            val alcohol = format.parse(alcoholValue)
-            val gas = format.parse(gasValue)
-            val formula = (
-                    alcohol?.toDouble() ?: alcoholValue.toDouble()) /
-                    (gas?.toDouble() ?: gasValue.toDouble()) < 0.7
-            result = if (!formula) {
-                "Gasolina"
-            } else if (formula) {
+            val alcohol = format.parse(alcoholValue)?.toDouble() ?: alcoholValue.toDouble()
+            val gas = format.parse(gasValue)?.toDouble() ?: gasValue.toDouble()
+            result = if (alcohol / gas < 0.7) {
                 "Álcool"
             } else {
-                "Dados Inválidos"
+                "Gasolina"
             }
         } catch (e: NumberFormatException) {
             result = "Dados Inválidos"
         }
         return result
+    }
+
+    fun specificCalculate(
+        alcoholValue: String,
+        alcoholDistance: String,
+        gasValue: String,
+        gasDistance: String
+    ): List<String> {
+        val format: NumberFormat = NumberFormat.getInstance(Locale.getDefault())
+        var result: String
+        var alcoholPerformance = 0.0
+        var gasPerformance = 0.0
+        try {
+            val alcoholPrice = format.parse(alcoholValue)?.toDouble() ?: alcoholValue.toDouble()
+            val alcoholDis = format.parse(alcoholDistance)?.toDouble() ?: alcoholDistance.toDouble()
+            val gasPrice = format.parse(gasValue)?.toDouble() ?: gasValue.toDouble()
+            val gasDis = format.parse(gasDistance)?.toDouble() ?: gasDistance.toDouble()
+            alcoholPerformance = alcoholPrice / alcoholDis
+            gasPerformance = gasPrice / gasDis
+            result = when {
+                alcoholPerformance < gasPerformance -> "Álcool"
+                alcoholPerformance > gasPerformance -> "Gasolina"
+                else -> "Idem"
+            }
+        } catch (e: NumberFormatException) {
+            result = "Dados Inválidos"
+        }
+        return listOf(result, alcoholPerformance.toString(), gasPerformance.toString())
     }
 }
